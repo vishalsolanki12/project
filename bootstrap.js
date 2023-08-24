679a6693ef7b55fc834f0c720ec234632c62905e
vishalsolanki12 <127103968+vishalsolanki12@users.noreply.github.com>
2023-08-24 13:30:32 +0530

    Add files via upload

---
+++ bootstrap.js
@@ -0,0 +1,19 @@
+star = document.getElementById('star');
+        star1 = document.getElementById('star1');
+
+        navlinks = document.getElementById('navlinks');
+
+        function test () 
+        {
+            star.style.display = "none";
+            star1.style.display = "block";
+            navlinks.style.display = "block";
+
+        }
+
+        function test1 ()
+        {
+            star.style.display = "block";
+            star1.style.display = "none";
+            navlinks.style.display = "none";
+}
